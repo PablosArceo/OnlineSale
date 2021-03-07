9b3e51064ca09648c962174977071c1d2299471b
Parceo2016416 <parceo-2016416@kinal.edu.gt>
2021-03-07 01:45:27 -0600

    Implementación de Categoria y Productos

---
+++ src/modelos/categoria.model.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var categoriaShema = Schema({
+nombreCategoria: String,
+productos:[{type: Schema.Types.ObjectId, ref:'producto'}]
+
+});
+
+module.exports = mongoose.model('categoria',categoriaShema);
