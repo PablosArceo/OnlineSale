cfb1e6c35ed675850fb5403acc66c2ef2ad10231
Parceo2016416 <parceo-2016416@kinal.edu.gt>
2021-03-06 19:31:34 -0600

    Gestión de Usuario

--- src/controladores/usuario.controlador.js
+++ src/controladores/usuario.controlador.js
@@ -6,9 +6,9 @@ var Usuario = require('../modelos/usuario.model');
 const { param } = require('../rutas/usuario.rutas');
 
 
-// Login de Admin
+// Login General
 
-function loginAdmin(req, res){
+function login(req, res){
     var params = req.body;
     
     Usuario.findOne({ user: params.user }, (err,  usuarioEncontrado)=>{
@@ -38,10 +38,12 @@ function loginAdmin(req, res){
 
 
     
-   // Agregar Clientes - Unicamente para Clientes
+   // Registrarse Como Cliente Nuevo
 function registrarCliente(req,res) {
 var usuarioModel = new Usuario;
 var params = req.body;
+var idCliente = req.params.idCliente;
+
 
 if (params.user && params. password){
     usuarioModel.user=params.user;
@@ -77,44 +79,50 @@ if (params.user && params. password){
 }
 }
 
-// Login de Cliente Unicamente para Clientes 
-function loginCliente (req,res){
-    var params = req.body;
 
-    Usuario.findOne({ user: params.user }, (err,  ClienteEncontrado)=>{
-        if (err) return res.status(500).send({ mensaje: 'Error en la peticion'});
-        
-        if (ClienteEncontrado){                         
-               bcrypt.compare(params.password, ClienteEncontrado.password, (err, passwordCorrect)=> {
-                   if(passwordCorrect){
-                       if(params.obtenerToken === 'true'){
-                           return res.status(200).send({
-                           token: jwt.createToken(ClienteEncontrado)
-                       });
-        
-                   }else{
-                       ClienteEncontrado.password = undefined;
-                       return res.status(200).send({ ClienteEncontrado })
-                   }
-               }else{
-                   return res.status(404).send({ mensaje: 'El Cliente no se ha podedido identificar'})
-               }
-        })
-        }else{
-            return res.status(404).send({mensaje: 'EL Cliente no se ha podido ingresar'})
-        }
-        })
-        }
+
     
+    // Editar Perfil De Cliente
+        function editarCliente(req, res) {
+             var params = req.body;
+             
+             if (req.user.rol != 'ROL_CLIENTE') return res.status(500).send({mensaje:'Solo clientes pueden modificar sus datos'})
+             
+            
+              Usuario.findByIdAndUpdate(req.user.sub, params, { new: true }, (err, ClienteActualizado)=>{ 
+               if(err) return status(500).send({mensaje: 'Error en la peticion'});
+               if(!ClienteActualizado) return res.status(500).send({ mensaje: 'No se ha podido actualizar el cliente'})
+  
+              return res.status(200).send({ ClienteActualizado });
+
+              } )
+            }
+
+    // Eliminar Perfil Para Clientes
+
+   function eliminarCliente(req, res) {
+             var params = req.body;
+             
+             if (req.user.rol != 'ROL_CLIENTE') return res.status(500).send({mensaje:'Solo clientes pueden borrar su perfil'})
+             
+             Usuario.findByIdAndDelete(req.user.sub, (err, ClienteEliminado)=>{
+                if(err) return res.status(500).send({ mensaje: 'Error en la peticion de Eliminar'});
+                if(!ClienteEliminado) return res.status(500).send({mensaje: 'Cliente eliminado con anterioridad'});
+           
+                return res.status(200).send({ ClienteEliminado});
+            })
+            }
+        
 
 
-     
-    /// Agregar Clientes y Administradores -- Unicamente Para Administradores --
+                                              // =====  FUNCIONES DE ADMINISTRADOR ==== // 
+
+     /// Agregar Clientes y Administradores -- Unicamente Para Administradores --
     function registrarNuevoUsuario(req,res) {
         var usuarioModel = new Usuario();
         var params = req.body;
 
-        if (req.user.rol != 'ROL_ADMIN') return res.status(500).send({mensaje:'Solo Admins pueden realizar dicha accion'})
+        if (req.user.rol != 'ROL_ADMIN') return res.status(500).send({mensaje:'Solo Administradores pueden realizar dicha accion'})
 
         if (params.user && params.password && params.rol){
         usuarioModel.user = params.user;
@@ -154,35 +162,85 @@ function loginCliente (req,res){
     }
     }
 
-     function editarCliente(req, res) {
-           var idCliente = req.params.idCliente;
-            var params = req.body;
-            
-            if (req.user.rol != 'ROL_CLIENTE') return res.status(500).send({mensaje:'Solo clientes pueden modificar sus datos'})
-            
-            Usuario.findById(req.user.sub, (err,ClienteEncontrado)=>{
-                    if(err) return res.status(500).send({mensaje: 'Error en la busqueda de Cliente'})
-                    if(ClienteEncontrado){
-                          if (ClienteEncontrado.user == req.user.usuario){
-                              Usuario.updateOne(ClienteEncontrado,params, {new: true}, (err,ClienteActualizado)=>{
-                              if(err) return res.status(500).send({mensaje:'Error general en la peticion'})
-                              if(!ClienteActualizado) return res.status(500).send({mensaje: 'Cliente no actualizado, revise los datos'})
-                              return res.status(200).send({mensaje: 'Cliente Actualizado'})
-                              })
-                          }else{
-                              return res.status(500).send({mensaje:'Cliente actualizado ya con aterioridad'})
-                          }   
-                    }
-            })
-            }
-  
- 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //  Modificar Roles  --- Unicamente para Administradores ---  
+   function editarRoles(req,res){
+   var idUser = req.params.idUser
+   var params = req.body
+
+   if (req.user.rol != 'ROL_ADMIN') return res.status(500).send({mensaje:'Solo Administradores pueden realizar dicha accion'})
+   
+   Usuario.findByIdAndUpdate({_id: idUser, rol: 'ROL_CLIENTE'}, {rol: params.rol}, {new: true, useFindAndModify: false}, (err, RolActualizado)=>{
+   if(err) return res.status(500).send({mensaje: 'Error en la peticion'});
+   if(!RolActualizado) return res.status(500).send({mensaje: 'Usuario no encontrado o es un usuario tipo Administrador'});
+   return res.status(500).send({ 'Cliente con nuevo Rol': RolActualizado});
+   });
+
+}
+
+
+
+   // Editar clientes Logeado como Admin
+
+   function editarUser(req, res) {
+   var idUser = req.params.idUser
+   var params = req.body;
+    
+    if (req.user.rol != 'ROL_ADMIN') return res.status(500).send({mensaje:'Solo Administradores pueden realizar dicha accion'})
+    
+    Usuario.findByIdAndUpdate({_id: idUser, rol: 'ROL_CLIENTE'}, {user: params.user},{new: true, useFindAndModify: false}, (err,ClienteActualizado)=>{
+    if(err) return res.status(500).send({mensaje: 'Error en la peticion'});
+    if(!ClienteActualizado) return res.status(500).send({mensaje: 'Cliente no encontrado o es un usuario tipo Administrador'});
+    return res.status(500).send({'Cliente con Nuevo Nombre': ClienteActualizado});
+  
+    });
+     
+   }
+
+
+   // Eliminar Clientes Logeado como Admin 
+   function eliminarUser(req,res){
+   var idUser = req.params.idUser;
+   var params = req.body;
+
+   if (req.user.rol != 'ROL_ADMIN') return res.status(500).send({mensaje: 'Solo Administradores pueden realizar dicha accion'})
+
+   Usuario.findByIdAndDelete({_id: idUser, rol: 'ROL_CLIENTE'}, (err, ClienteEliminado)=>{
+
+   if(err) return res.status(500).send({mensaje: 'Error en la peticion'});
+   if(!ClienteEliminado) return res.status(500).send({mensaje: 'Cliente no encontrado o es un usuario tipo Administrador, '});
+   return res.status(500).send({'Cliente Eliminado': ClienteEliminado});   
+   });
+
+   }
+
+
+
+    
 module.exports={
-loginAdmin,
+login,
 registrarCliente,
-loginCliente,
 registrarNuevoUsuario,
-editarCliente
+editarCliente,
+eliminarCliente,
+editarUser,
+eliminarUser,
+editarRoles
+
 
     }
--- src/rutas/usuario.rutas.js
+++ src/rutas/usuario.rutas.js
@@ -7,9 +7,20 @@ const md_autentication = require('../middlewares/authenticated');
 
 const api = express.Router();
 
-api.post('/loginAdmin', usuarioControlador.loginAdmin );
-api.post('/registrarCliente',usuarioControlador.registrarCliente );
-api.post('/loginCliente',usuarioControlador.loginCliente);
-api.put('/editarCliente',md_autentication.ensureAuth, usuarioControlador.editarCliente);
+//     === Login  General ===
+api.post('/login', usuarioControlador.login );
+
+
+//      === Administrador ===
 api.post('/registrarNuevoUsuario',md_autentication.ensureAuth,usuarioControlador. registrarNuevoUsuario);
+api.put('/editarUser/:idUser',md_autentication.ensureAuth,usuarioControlador.editarUser);
+api.delete('/eliminarUser/:idUser', md_autentication.ensureAuth, usuarioControlador.eliminarUser);
+api.put('/editarRoles/:idUser', md_autentication.ensureAuth,usuarioControlador.editarRoles);
+
+
+//      === Cliente ===
+api.post('/registrarCliente',usuarioControlador.registrarCliente );
+api.put('/editarCliente',md_autentication.ensureAuth, usuarioControlador.editarCliente);
+api.delete('/eliminarCliente',md_autentication.ensureAuth,usuarioControlador.eliminarCliente);
+
 module.exports = api;
