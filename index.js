0b817b660461833ea837443ad80007e8f397279d
Parceo2016416 <parceo-2016416@kinal.edu.gt>
2021-03-12 11:43:34 -0600

    Proyecto Completado

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const mongoose = require("mongoose");
 const app = require('./app');
 const Usuario = require('./src/modelos/usuario.model');
-const Categoria = require('./src/modelos/categoria.model');
 const bcrypt = require('bcrypt-nodejs');
 
 
@@ -49,30 +48,7 @@ mongoose.connect('mongodb://localhost:27017/ProyectoFinal',{ useNewUrlParser: tr
 
     }  )
    
-    /*  var categoria = new Categoria;
-        
-    
-        Categoria.findOne({nombreCategoria:'default'},(err, categoriaEncontrada)=>{
-            if(err){
-              return console.log('Error en la petición');
-            }else if(categoriaEncontrada){
-              return console.log('Categoria default ya existente');
-            }else{
-                categoria.nombreCategoria ='default';
 
-                categoria.save((err, CategoriaGuardada)=>{
-                    if(err){
-                      return console.log({mensaje:'Error al guardar categoria.'});
-                    }else if(CategoriaGuardada){
-                        return console.log({CategoriaGuardada});
-                    }else{
-                      return console.log('Categoria no guardada.');
-                    }
-                });
-            }
-        });
-    
- */
    
     app.listen(3000, function(){
         console.log('El servidor esta arrancando en el puerto: 3000')
